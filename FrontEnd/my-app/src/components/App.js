d9a15d33c379407e4adb157113df50f0c44a94ff
yassine <yassine.afif.2002@gmail.com>
2024-04-11 01:59:31 +0000

    this is update if Project until dashboard Prof not work and i just make update to api noValideProfDoctorant(Long id)

---
+++ FrontEnd/my-app/src/components/App.js
@@ -0,0 +1,39 @@
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../theme/theme'; // Correct the import path to theme.js
+
+// Component imports
+import TeamPage from './Home/TeamPage';
+import Header from './common/Header'; // Correct the import path to Header.js
+import Footer from './common/Footer';
+import AboutUs from './Home/AboutUs'; // Correct the import path to AboutUs.js
+import ContactUs  from './Home/ContactUs'; // Correct the import path to ContactUs.js
+import RegistrationForm  from './Home/RegistrationForm'; // Correct the import path to ContactUs.js
+import SignUp  from './Home/SignUp';
+import Dashboard  from './Dashboard/Dashboard';
+
+
+function App() {
+    return (
+        <ThemeProvider theme={theme}>
+            <Router>
+                <Header /> {/* Header will appear on all pages */}
+                <Routes>
+                    <Route path="/about-us" element={<AboutUs />} />
+                    <Route path="/team" element={<TeamPage />} /> {/* Add this line */}
+                    <Route path="/contact" element={<ContactUs />} />
+                    <Route path="/login" element={<RegistrationForm />} />
+                    <Route path="/signup" element={<SignUp />} />
+                    <Route path="/Dashboard" element={<Dashboard />} />
+                    {/* Add additional routes for other pages */}
+                </Routes>
+                <Footer />
+                {/* Place other components that should render on all pages here */}
+            </Router>
+        </ThemeProvider>
+    );
+}
+
+export default App;
